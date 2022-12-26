0a465c6e5a8e4eab49d22a33ca1bd6585db16adc
Hamed Aghabalazadeh <Hamed@Hameds-MacBook-Pro.local>
2022-12-26 20:13:58 +0330

    Updates

---
+++ precache-manifest.eb6e9fff5801729c47f490f1ea981884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91b3741455b31f5f5d37a936d41ac23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d754e40fb1a5508b1215",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "0bb06af31f677904fe9e",
+    "url": "/static/css/main.b3a86454.chunk.css"
+  },
+  {
+    "revision": "d754e40fb1a5508b1215",
+    "url": "/static/js/2.43af1692.chunk.js"
+  },
+  {
+    "revision": "0bb06af31f677904fe9e",
+    "url": "/static/js/main.2547eaf1.chunk.js"
+  },
+  {
+    "revision": "1319a952f0aa42344810",
+    "url": "/static/js/runtime-main.ce388e37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.125a21e50b61f52b16054dc5b09d5d74.js"
+  "/precache-manifest.eb6e9fff5801729c47f490f1ea981884.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
